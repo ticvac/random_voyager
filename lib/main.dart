aee3b8c5dac2c663c0ecd3073ca302efe7979fbb
ticvac <vaclav.tichy180@gmail.com>
2023-11-15 23:16:34 +0100

    barvy a font

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "File demo",
       theme: ThemeData.dark(),
       home: const MyHomePage(),
@@ -24,15 +25,72 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+Color backgroundColor = const Color(0xff122332);
+Color coolYellow = const Color(0xfffbc10a);
+Color darkerBlue = const Color(0xff091521);
+
+TextStyle buttonTextStyle = TextStyle(
+  fontSize: 25,
+  fontFamily: "hand_mono",
+  color: Colors.white,
+);
+
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: backgroundColor,
       appBar: AppBar(
-        title: Text("File demo"),
+        title: Text(
+          "Random Voyager",
+          style: TextStyle(
+              fontSize: 30,
+              fontFamily: "hand_mono",
+              color: Colors.white
+          ),
+        ),
+        backgroundColor: darkerBlue,
       ),
-      body: Container(
-
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Spacer(flex: 10,),
+            Text(
+              "Where to go?",
+              style: TextStyle(
+                color: coolYellow,
+                fontSize: 40,
+                fontFamily: "hand_mono",
+              ),
+            ),
+            Spacer(flex: 2,),
+            TextButton(
+              onPressed: () {},
+              child: Text(
+                "   Quick & Random ->   ",
+                style: buttonTextStyle,
+              ),
+            ),
+            Spacer(),
+            TextButton(
+              onPressed: () {},
+              child: Text(
+                "   Nice places ->   ",
+                style: buttonTextStyle,
+              ),
+            ),
+            Spacer(),
+            TextButton(
+              onPressed: () {},
+              child: Text(
+                "   My voyges ->   ",
+                style: buttonTextStyle,
+              ),
+            ),
+            Spacer(flex: 15,),
+          ],
+        ),
       ),
     );
   }
